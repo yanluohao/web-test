c88d2836ca6e06409cb283a5bef29a4c00724ebb
yanluohao <345072986@qq.com>
2016-08-12 18:08:43 +0800

    更新权限
    设定一个权限可以浏览所有的漫画

--- 漫画页面/js/cartoon.js
+++ 漫画页面/js/cartoon.js
@@ -1,68 +1,73 @@
 window.onload = function() {
-    //判断入职天数根据天数加载不同的内容;
-    if (localStorage.popup_counts == undefined) {
-        localStorage.popup_counts = "0";
-    }
-    //获取当前日期的对应时间具体到时分秒;
-    function if_newDay() {
-        function getNewTime() {
-            var getnewTime = new Date();
-            var getnewHours = getnewTime.getHours();
-            var getnewMinutes = getnewTime.getMinutes();
-            var getnewSeconds = getnewTime.getSeconds();
-            if (getnewHours == 0 && getnewMinutes == 0 && getnewSeconds == 1) {
-                localStorage.popup_counts = "0";
-            }
+        //判断入职天数根据天数加载不同的内容;
+        if (localStorage.popup_counts == undefined) {
+            localStorage.popup_counts = "0";
         }
-        timer = setInterval(getNewTime, 1000);
-    }
-    if_newDay();
-    if (day == 1) {
-        if (localStorage.popup_counts.indexOf("0") != -1) {
-            //跳转到漫画起始观看页
-            $(".popcover").after('<div id="popup1"></div>');
-            var popup = document.getElementById("popup1");
-            var newbiePic = document.createElement("img");
-            newbiePic.className = "newbiepic";
-            newbiePic.src = "images/firstDay.png";
-            popup.appendChild(newbiePic);
-            var cancelbtn1 = document.createElement("img");
-            cancelbtn1.className = "cancelbtn1";
-            cancelbtn1.src = "images/cancelbtn.png";
-            cancelbtn1.setAttribute("onclick", "common.cancelPop(this)");
-            popup.appendChild(cancelbtn1);
-            var enterbtn = document.createElement("img");
-            enterbtn.className = "enterbtn";
-            enterbtn.src = "images/enterbtn.png";
-            enterbtn.setAttribute("onclick", "firstday.showCartoon(this)");
-            popup.appendChild(enterbtn);
-            localStorage.popup_counts = localStorage.popup_counts.replace("0", "1");
-            if (wHeight <= 800&&wHeight>=700) {
-                $(".newbiepic").css("width", "550px");
+        //获取当前日期的对应时间具体到时分秒;
+        function if_newDay() {
+            function getNewTime() {
+                var getnewTime = new Date();
+                var getnewHours = getnewTime.getHours();
+                var getnewMinutes = getnewTime.getMinutes();
+                var getnewSeconds = getnewTime.getSeconds();
+                if (getnewHours == 0 && getnewMinutes == 0 && getnewSeconds == 1) {
+                    localStorage.popup_counts = "0";
+                }
             }
-            else if(wHeight<700){
-                $(".newbiepic").css("width", "480px");
+            timer = setInterval(getNewTime, 1000);
+        }
+        if_newDay();
+        if (!watchAll) {
+            if (day == 1) {
+                if (localStorage.popup_counts.indexOf("0") != -1) {
+                    //跳转到漫画起始观看页
+                    $(".popcover").after('<div id="popup1"></div>');
+                    var popup = document.getElementById("popup1");
+                    var newbiePic = document.createElement("img");
+                    newbiePic.className = "newbiepic";
+                    newbiePic.src = "images/firstDay.png";
+                    popup.appendChild(newbiePic);
+                    var cancelbtn1 = document.createElement("img");
+                    cancelbtn1.className = "cancelbtn1";
+                    cancelbtn1.src = "images/cancelbtn.png";
+                    cancelbtn1.setAttribute("onclick", "common.cancelPop(this)");
+                    popup.appendChild(cancelbtn1);
+                    var enterbtn = document.createElement("img");
+                    enterbtn.className = "enterbtn";
+                    enterbtn.src = "images/enterbtn.png";
+                    enterbtn.setAttribute("onclick", "firstday.showCartoon(this)");
+                    popup.appendChild(enterbtn);
+                    localStorage.popup_counts = localStorage.popup_counts.replace("0", "1");
+                    if (wHeight <= 800 && wHeight >= 700) {
+                        $(".newbiepic").css("width", "550px");
+                    } else if (wHeight < 700) {
+                        $(".newbiepic").css("width", "480px");
+                    }
+                } else {
+                    $(".popcover").hide();
+                    $(".cartoonsmpic").fadeIn();
+                }
+            } else if (day > 1 && day <= 90) {
+                //if条件内day<=的数值需要做修改根据漫画的天数修改
+                if (localStorage.popup_counts.indexOf("0") != -1) {
+                    common.showCartoon();
+                    localStorage.popup_counts = localStorage.popup_counts.replace("0", "1");
+                } else {
+                    $(".popcover").hide();
+                    $(".cartoonsmpic").fadeIn();
+                }
+            } else {
+                $(".popcover").hide();
             }
         } else {
             $(".popcover").hide();
             $(".cartoonsmpic").fadeIn();
+
         }
-    } else if (day > 1 && day <= 90) {
-        //if条件内day<=的数值需要做修改根据漫画的天数修改
-        if (localStorage.popup_counts.indexOf("0") != -1) {
-            common.showCartoon();
-            localStorage.popup_counts = localStorage.popup_counts.replace("0", "1");
-        } else {
-            $(".popcover").hide();
-            $(".cartoonsmpic").fadeIn();
-        }
-    } else {
-        $(".popcover").hide();
+        //清理本地缓存
+        //localStorage.removeItem("popup_counts");
     }
-    //清理本地缓存
-    localStorage.removeItem("popup_counts");
-}
-//判断浏览器的分辨率,兼容1366分辨率的老土做法;
+    //判断浏览器的分辨率,兼容1366分辨率的老土做法;
 var wHeight = $(window).height();
 //用户注册日期与系统日期之间的差
 // 对Date的扩展，将 Date 转化为指定格式的String
@@ -72,22 +77,22 @@ var wHeight = $(window).height();
 // (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423 
 // (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18 
 Date.prototype.Format = function(fmt) { //author: meizz 
-        var o = {
-            "M+": this.getMonth() + 1, //月份 
-            "d+": this.getDate(), //日 
-            "h+": this.getHours(), //小时 
-            "m+": this.getMinutes(), //分 
-            "s+": this.getSeconds(), //秒 
-            "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-            "S": this.getMilliseconds() //毫秒 
-        };
-        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-        for (var k in o)
-            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-        return fmt;
-    }
-    //计算天数差
-var register_day = "2016-08-01";
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
+var register_day = "2016-08-12";
 var time_today = new Date().Format("yyyy-MM-dd");
 var year_1 = register_day.substr(0, 4);
 var year_2 = time_today.substr(0, 4);
@@ -103,9 +108,20 @@ temp_2 = year_2 + "/" + month_2 + "/" + day_2;
 var dateaa = new Date(temp_1);
 var datebb = new Date(temp_2);
 var datedifference = datebb.getTime() - dateaa.getTime();
+//漫画目前的话数
+var cartoonLength = 7;
+//计算天数差
 var day = Math.floor(datedifference / (1000 * 60 * 60 * 24)) + 1;
-var chapter_look=Math.ceil(day/3);
-var minDay=day;
+//等于true时拥有查看所有的权限;
+var watchAll = false;
+if (watchAll) {
+    chapter_look = cartoonLength;
+} else {
+    chapter_look = Math.ceil(day / 3);
+};
+
+
+var minDay = day;
 
 var list_page = 0;
 //弹窗消失，小图显示
@@ -172,27 +188,52 @@ var common = {
             $(".cartoonPages span").eq(2).html(nowdayLength);
             if (picId >= nowdayLength) {
                 //if条件内day<=的数值需要做修改根据漫画的天数修改
-                if (chapter_look >= Math.ceil(day/3)) {
+                if (!watchAll) {
+                    if (chapter_look >= Math.ceil(day / 3)) {
 
-                } else {
-                    chapter_look++;
-                    var dayArr = [];
-                    for (var k = 0; k < dataint.length; k++) {
-                        if (dataint[k].day == chapter_look) {
-                            dayArr.push(dataint[k]);
+                    } else {
+                        chapter_look++;
+                        var dayArr = [];
+                        for (var k = 0; k < dataint.length; k++) {
+                            if (dataint[k].day == chapter_look) {
+                                dayArr.push(dataint[k]);
+                            }
+                        }
+                        dayLength = dayArr.length;
+                        $(".cartoonPages span").eq(2).html(dayLength);
+                        picId = 1;
+                        for (var j = 0; j < dataint.length; j++) {
+                            if (dataint[j].day == chapter_look && dataint[j].id == picId) {
+                                $(".cartoonbox>img").attr("src", dataint[j].src);
+                                $(".cartoonbox>img").attr("name", dataint[j].id);
+                                $(".cartoonPages span").eq(0).html(dataint[j].id);
+                            }
                         }
                     }
-                    dayLength = dayArr.length;
-                    $(".cartoonPages span").eq(2).html(dayLength);
-                    picId = 1;
-                    for (var j = 0; j < dataint.length; j++) {
-                        if (dataint[j].day == chapter_look && dataint[j].id == picId) {
-                            $(".cartoonbox>img").attr("src", dataint[j].src);
-                            $(".cartoonbox>img").attr("name", dataint[j].id);
-                            $(".cartoonPages span").eq(0).html(dataint[j].id);
+                } else {
+                    if (chapter_look >= cartoonLength) {
+
+                    } else {
+                        chapter_look++;
+                        var dayArr = [];
+                        for (var k = 0; k < dataint.length; k++) {
+                            if (dataint[k].day == chapter_look) {
+                                dayArr.push(dataint[k]);
+                            }
+                        }
+                        dayLength = dayArr.length;
+                        $(".cartoonPages span").eq(2).html(dayLength);
+                        picId = 1;
+                        for (var j = 0; j < dataint.length; j++) {
+                            if (dataint[j].day == chapter_look && dataint[j].id == picId) {
+                                $(".cartoonbox>img").attr("src", dataint[j].src);
+                                $(".cartoonbox>img").attr("name", dataint[j].id);
+                                $(".cartoonPages span").eq(0).html(dataint[j].id);
+                            }
                         }
                     }
                 }
+
             } else {
                 picId++;
                 for (var i = 0; i < dataint.length; i++) {
@@ -226,31 +267,31 @@ var common = {
             */
             var showImg1 = document.createElement("img");
             //showImg1.setAttribute("onclick", "common.openthisCartoon(this)");
-            showImg1.src = "images/showthisPic.png";
+            showImg1.src = "images/notshowthis.png";
             cartoonItem.appendChild(showImg1);
             var showImg2 = document.createElement("img");
             //showImg2.setAttribute("onclick", "common.openthisCartoon(this)");
-            showImg2.src = "images/showthisPic.png";
+            showImg2.src = "images/notshowthis.png";
             cartoonItem.appendChild(showImg2);
             var showImg3 = document.createElement("img");
             //showImg3.setAttribute("onclick", "common.openthisCartoon(this)");
-            showImg3.src = "images/showthisPic.png";
+            showImg3.src = "images/notshowthis.png";
             cartoonItem.appendChild(showImg3);
             var showImg4 = document.createElement("img");
             //showImg4.setAttribute("onclick", "common.openthisCartoon(this)");
-            showImg4.src = "images/showthisPic.png";
+            showImg4.src = "images/notshowthis.png";
             cartoonItem.appendChild(showImg4);
             var showImg5 = document.createElement("img");
             //showImg5.setAttribute("onclick", "common.openthisCartoon(this)");
-            showImg5.src = "images/showthisPic.png";
+            showImg5.src = "images/notshowthis.png";
             cartoonItem.appendChild(showImg5);
             var showImg6 = document.createElement("img");
             //showImg6.setAttribute("onclick", "common.openthisCartoon(this)");
-            showImg6.src = "images/showthisPic.png";
+            showImg6.src = "images/notshowthis.png";
             cartoonItem.appendChild(showImg6);
             var showImg7 = document.createElement("img");
             //showImg7.setAttribute("onclick", "common.openthisCartoon(this)");
-            showImg7.src = "images/showthisPic.png";
+            showImg7.src = "images/notshowthis.png";
             cartoonItem.appendChild(showImg7);
             var itemBtn = document.createElement("div");
             itemBtn.className = "itemBtn";
@@ -263,21 +304,16 @@ var common = {
             listNext.className = "listNext";
             listNext.src = "images/listNext.png";
             itemBtn.appendChild(listNext);
-            for(var q=0;q<Math.ceil(day/3);q++){
-                $(".cartoonItembox>img").eq(q).attr("onclick","common.openthisCartoon(this)");
-            };
-            if (wHeight <= 800&&wHeight>=700) {
-                $(".cartoonbox").css("width", "340px");
-                $(".cartoonItembox").css("width", "400px");
-                $(".cartoonItembox").css("height", "592px");
-                $(".cartoonItembox").css("background-size", "cover");
-                $(".cartoonItembox>img").css("margin", "0 0 12px 270px");
-                $(".cartoonItembox>img").eq(0).css("margin", "105px 0 14px 270px");
-                $(".cartoonItembox>img").eq(6).css("margin-bottom", "6px");
-                $(".listPrev").css({"height":"50px","width":"50px"});
-                $(".listNext").css({"height":"50px","width":"50px","margin-left":"160px"});
+            if (!watchAll) {
+                for (var q = 0; q < Math.ceil(day / 3); q++) {
+                    $(".cartoonItembox>img").eq(q).attr({"onclick":"common.openthisCartoon(this)","src":"images/showthisPic.png"});
+                }
+            } else {
+                for (var q = 0; q < 7; q++) {
+                    $(".cartoonItembox>img").eq(q).attr({"onclick":"common.openthisCartoon(this)","src":"images/showthisPic.png"});
+                }
             }
-            else if(wHeight<700){
+            if (wHeight <= 800 && wHeight >= 700) {
                 $(".cartoonbox").css("width", "340px");
                 $(".cartoonItembox").css("width", "400px");
                 $(".cartoonItembox").css("height", "592px");
@@ -285,8 +321,18 @@ var common = {
                 $(".cartoonItembox>img").css("margin", "0 0 12px 270px");
                 $(".cartoonItembox>img").eq(0).css("margin", "105px 0 14px 270px");
                 $(".cartoonItembox>img").eq(6).css("margin-bottom", "6px");
-                $(".listPrev").css({"height":"50px","width":"50px"});
-                $(".listNext").css({"height":"50px","width":"50px","margin-left":"160px"});
+                $(".listPrev").css({ "height": "50px", "width": "50px" });
+                $(".listNext").css({ "height": "50px", "width": "50px", "margin-left": "160px" });
+            } else if (wHeight < 700) {
+                $(".cartoonbox").css("width", "340px");
+                $(".cartoonItembox").css("width", "400px");
+                $(".cartoonItembox").css("height", "592px");
+                $(".cartoonItembox").css("background-size", "cover");
+                $(".cartoonItembox>img").css("margin", "0 0 12px 270px");
+                $(".cartoonItembox>img").eq(0).css("margin", "105px 0 14px 270px");
+                $(".cartoonItembox>img").eq(6).css("margin-bottom", "6px");
+                $(".listPrev").css({ "height": "50px", "width": "50px" });
+                $(".listNext").css({ "height": "50px", "width": "50px", "margin-left": "160px" });
             }
         },
         openthisCartoon: function(obj) {
@@ -349,10 +395,9 @@ var common = {
             imgNext.src = "images/cartoonNext.png";
             imgNext.setAttribute("onclick", "common.cartoonNext()");
             cartoonbtnBox.appendChild(imgNext);
-            if (wHeight <= 800&&wHeight>=700) {
+            if (wHeight <= 800 && wHeight >= 700) {
                 $(".cartoonbox").css("width", "380px");
-            }
-            else if(wHeight<700){
+            } else if (wHeight < 700) {
                 $(".cartoonbox").css("width", "340px");
             }
         },
@@ -427,10 +472,9 @@ var common = {
             imgNext.src = "images/cartoonNext.png";
             imgNext.setAttribute("onclick", "common.cartoonNext()");
             cartoonbtnBox.appendChild(imgNext);
-            if (wHeight <= 800&&wHeight>=700) {
+            if (wHeight <= 800 && wHeight >= 700) {
                 $(".cartoonbox").css("width", "380px");
-            }
-            else if(wHeight<700){
+            } else if (wHeight < 700) {
                 $(".cartoonbox").css("width", "340px");
             }
         },
@@ -492,10 +536,9 @@ var common = {
             imgNext.src = "images/cartoonNext.png";
             imgNext.setAttribute("onclick", "common.cartoonNext()");
             cartoonbtnBox.appendChild(imgNext);
-            if (wHeight <= 800&&wHeight>=700) {
+            if (wHeight <= 800 && wHeight >= 700) {
                 $(".cartoonbox").css("width", "380px");
-            }
-            else if(wHeight<700){
+            } else if (wHeight < 700) {
                 $(".cartoonbox").css("width", "340px");
             }
         }
@@ -553,10 +596,9 @@ var firstday = {
             imgNext.src = "images/cartoonNext.png";
             imgNext.setAttribute("onclick", "common.cartoonNext()");
             cartoonbtnBox.appendChild(imgNext);
-            if (wHeight <= 800&&wHeight>700) {
+            if (wHeight <= 800 && wHeight > 700) {
                 $(".cartoonbox").css("width", "380px");
-            }
-            else if(wHeight<700){
+            } else if (wHeight < 700) {
                 $(".cartoonbox").css("width", "340px");
             }
         }
